907a09bd50c1265fc365bf2fa0cdb58d0d8431fa
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-10-18 17:42:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,22 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
     ]]
 )
 
-
+async def send_msg(user_id, message):
+	try:
+		await message.copy(chat_id=user_id)
+		return 200, None
+	except FloodWait as e:
+		await asyncio.sleep(e.x)
+		return send_msg(user_id, message)
+	except InputUserDeactivated:
+		return 400, f"{user_id} : deactivated\n"
+	except UserIsBlocked:
+		return 400, f"{user_id} : user is blocked\n"
+	except PeerIdInvalid:
+		return 400, f"{user_id} : user id invalid\n"
+	except Exception as e:
+		return 500, f"{user_id} : {traceback.format_exc()}\n"
+		
 
 @Bot.on_callback_query()
 async def cb_handler(bot, update):
