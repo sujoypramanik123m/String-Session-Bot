1db7c01bf67dcdc5d4b4854399a87439f6cdbd4d
CodeXBots <RahulReviewsYT@gmail.com>
2024-11-12 16:37:13 +0530

    Update

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_HASH = os.getenv("API_HASH", "")
+API_ID = int(os.getenv("API_ID", ""))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "RahulReviewsYT")
+BOT_OWNER = int(os.getenv("BOT_OWNER", ""))
+DATABASE_URL = os.getenv("DATABASE_URL", "")
