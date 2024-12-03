8d3adaa64282807dfc6122e0e44fdca0919fbb1a
Techify Bots <RahulReviewsYT@gmail.com>
2024-12-03 15:59:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 API_HASH = os.getenv("API_HASH", "")
 API_ID = int(os.getenv("API_ID", ""))
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "RahulReviewsYT")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "TechifyBots")
 BOT_OWNER = int(os.getenv("BOT_OWNER", ""))
 DATABASE_URL = os.getenv("DATABASE_URL", "")
