db5c1a17d44ae623a5bea019ac7a905ea20a37ef
Techify Bots <TechifyBots@gmail.com>
2025-02-12 11:31:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-import os
+from os import environ
 
-API_HASH = os.getenv("API_HASH", "")
-API_ID = int(os.getenv("API_ID", ""))
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "TechifyBots")
-BOT_OWNER = int(os.getenv("BOT_OWNER", ""))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER_ID = int(environ.get("OWNER_ID", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
+PORT = int(environ.get('PORT', 8080))
+AUTH_CHANNELS = environ.get("AUTH_CHANNEL", "")
+AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
