3c3e6020442984482708f9967b78ca18032c0dcf
Techify Bots <TechifyBots@gmail.com>
2025-04-27 22:45:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,49 +1,10 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from config import *
-from plugins.db import db
+from .db import db
+from .fsub import get_fsub
 from pyrogram.errors import RPCError
 
-async def get_fsub(bot, message):
-    user_id = message.from_user.id
-    not_joined = []
-
-    for channel_id in AUTH_CHANNELS:
-        try:
-            member = await bot.get_chat_member(channel_id, user_id)
-
-            if member.status in ["left", "kicked", "restricted"]:
-                not_joined.append(channel_id)
-        
-        except RPCError:
-            not_joined.append(channel_id)
-
-    if not not_joined:
-        return True
-
-    buttons = []
-    for channel_id in not_joined:
-        try:
-            chat = await bot.get_chat(channel_id)
-            channel_link = chat.invite_link
-            
-            if not channel_link:
-                raise ValueError("No invite link available")
-
-        except Exception:
-            channel_link = "https://telegram.me/Techifybots"
-
-        buttons.append([InlineKeyboardButton(f"🔔 Join {chat.title}", url=channel_link)])
-
-    await message.reply(
-        f"👋 Hello {message.from_user.mention()}, Welcome!\n\n"
-        "📢 Exclusive Access Alert! ✨\n\n"
-        "To unlock all the amazing features I offer, please join our updates channels. "
-        "This helps us keep you informed and ensures top-notch service just for you! 😊\n\n"
-        "🚀 Join now and dive into a world of knowledge and creativity!",
-        reply_markup=InlineKeyboardMarkup(buttons),
-    )
-    return False
 
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot: Client, msg: Message):
@@ -53,8 +14,7 @@ async def start(bot: Client, msg: Message):
             chat_id=LOG_CHANNEL, 
             text=f"**#NewUser\n\n👤 {msg.from_user.mention}**\n\nID - `{msg.from_user.id}`"
         )
-    if not await get_fsub(bot, msg):
-        return
+    if IS_FSUB and not await get_fsub(bot, msg):return
 
     await bot.send_message(
         chat_id=msg.chat.id,
