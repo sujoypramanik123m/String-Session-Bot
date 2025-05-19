1040585016a219904c8b41b85f8826ebbe77443b
Techify Bots <TechifyBots@gmail.com>
2025-05-19 19:28:07 +0530

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,31 @@
+class text(object):
+  START = """<b>{},
+
+ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ
+
+nᴜꜱᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ sᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ sᴛʀɪɴɢ sᴇssɪᴏɴ
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+
+  LOG = """#New_User
+
+User: {}
+ID: {}"""
+  
+  ABOUT = """<b>📜 Cʜᴇᴄᴋ Aʙᴏᴜᴛ:
+  
+Lɪʙʀᴀʀʏ: Pʏʀᴏɢʀᴀᴍ 📚
+Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 🐍
+Sᴇʀᴠᴇʀ: Rᴇɴᴅᴇʀ 🌐
+Bᴜɪʟᴅ Sᴛᴀᴛᴜs: V1.3 🚀
+Sᴏᴜʀᴄᴇ Cᴏᴅᴇ: Aᴠᴀɪʟᴀʙʟᴇ (Fʀᴇᴇ) 💻 
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
+  
+  HELP = """<b>{},
+
+ᴛʜɪꜱ ɪꜱ ʀᴇᴀʟʟʏ sɪᴍᴘʟᴇ 😂
+
+ᴊᴜsᴛ ɢᴏ ʙᴀᴄᴋ ᴀɴᴅ ᴄʟɪᴄᴋ ᴏɴ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ʙᴜᴛᴛᴏɴ ᴀɴᴅ ᴛʜᴇɴ ꜰᴏʟʟᴏᴡ ᴛʜᴇ sᴛᴇᴘs
+
+<blockquote>ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/TechifyRahul'>ʀᴀʜᴜʟ</a></blockquote></b>"""
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from typing import List
+
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
+
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = os.environ.get("DB_NAME", "")
+
+IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
